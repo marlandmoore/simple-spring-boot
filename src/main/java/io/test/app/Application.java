e2eb2329911a7902b36239e91353b0f5557b7b76
marland moore <marland@evolvinglabs.com>
2020-08-06 19:24:22 -0400

    update

--- src/main/java/io/test/app/Application.java
+++ src/main/java/io/test/app/Application.java
@@ -8,12 +8,8 @@ import io.test.app.service.HelloService;
 @SpringBootApplication
 public class Application  {
 
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ){
         ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-		HelloService service =  context.getBean(HelloService.class);
-		System.out.println(service.getProperty());
-		context.close();
     }
      
 
